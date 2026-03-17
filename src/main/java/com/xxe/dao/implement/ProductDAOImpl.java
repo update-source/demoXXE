2a7614b74114ecb3546b0162f362110f9066200d
update-source <24162135@student.hcmute.edu.vn>
2026-03-17 10:38:00 +0700

    Refactor getAllProducts method in ProductDAOImpl to use try-with-resources for PreparedStatement and ResultSet

--- src/main/java/com/xxe/dao/implement/ProductDAOImpl.java
+++ src/main/java/com/xxe/dao/implement/ProductDAOImpl.java
@@ -23,18 +23,20 @@ public class ProductDAOImpl implements ProductDAO {
         db.connect();
         Connection conn = db.getConnection();
         String sqlQuery = "SELECT * FROM xxe.products";
-        PreparedStatement pstmt = conn.prepareStatement(sqlQuery);
-        ResultSet resultSet = pstmt.executeQuery();
-        while (resultSet.next()) {
-          int productId = resultSet.getInt(1);
-          String productName = resultSet.getString(2);
-          int productPrice = resultSet.getInt(3);
-          String productDescription = resultSet.getString(4);
-          int productStars = resultSet.getInt(5);
-          String productImgUrl = resultSet.getString(6);
-          int storeId = resultSet.getInt(7);
+        try (PreparedStatement pstmt = conn.prepareStatement(sqlQuery)) {
+          try (ResultSet resultSet = pstmt.executeQuery()) {
+            while (resultSet.next()) {
+              int productId = resultSet.getInt(1);
+              String productName = resultSet.getString(2);
+              int productPrice = resultSet.getInt(3);
+              String productDescription = resultSet.getString(4);
+              int productStars = resultSet.getInt(5);
+              String productImgUrl = resultSet.getString(6);
+              int storeId = resultSet.getInt(7);
 
-          list.add(new Product(productDescription, productImgUrl, productName, productPrice, productId, productStars, storeId));
+              list.add(new Product(productDescription, productImgUrl, productName, productPrice, productId, productStars, storeId));
+            }
+          }
         }
       } catch (SQLException e) {
         throw new RuntimeException("Failed to query products from database: " + e.getMessage(), e);
@@ -50,6 +52,7 @@ public class ProductDAOImpl implements ProductDAO {
 
     
     public static void main(String[] args) {
+      /* Test getAllProducts method */
       ProductDAO dao = new ProductDAOImpl();
       List<Product> list = dao.getAllProducts();
       System.out.println(list);
